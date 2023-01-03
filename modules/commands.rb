a2f61af2584528eb0d897aae1d77f5433a35fcbf
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2023-01-03 20:06:17 -0300

    standard

--- modules/commands.rb
+++ modules/commands.rb
@@ -1,4 +1,4 @@
-module Optimize
+module Command
   # Constants
 
   # Methods
@@ -6,7 +6,7 @@ module Optimize
   # z, Vec[x0, x1, block] => /fill || /setblock
   # 0, [0, 2, concrete 15] => "fill 0 50 0 2 50 0 concrete 15"
   # 1, [5, 5, concrete 0]  => "setblock 5 50 1 concrete 0"
-  def self.eval(z_coord, *args)
+  def self.to_command(z_coord, *args)
     if args[0] == args[1]
       return "setblock #{args[0]} #{DEFAULT_Y} #{z_coord} #{args[2]}"
     end
@@ -15,4 +15,5 @@ module Optimize
                 "#{args[1]} #{DEFAULT_Y} #{z_coord} #{args[2]}"
 
   end
+
 end
