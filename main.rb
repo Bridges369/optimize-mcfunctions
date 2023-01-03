aefc956a13589b73c64b6c615cf516d6d255d331
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2023-01-03 01:06:41 -0300

    refactored project tree

--- main.rb
+++ main.rb
@@ -1,29 +1,6 @@
-module Optmize
-  # Constants
-  WIDTH = 2         # z
-  HEIGHT = 5        # x
-  DEFAULT_Y = 50    # y
+require_relative 'modules/optimize'
 
-  # Methods
-
-  # z, Vec[x0, x1, block] => /fill || /setblock
-  # 0, [0, 2, concrete 15] => "fill 0 50 0 2 50 0 concrete 15"
-  # 1, [5, 5, concrete 0]  => "setblock 5 50 1 concrete 0"
-  def eval(z_coord, *args)
-    if args[0] == args[1]
-      return "setblock #{args[0]} #{DEFAULT_Y} #{z_coord} #{args[2]}"
-    end
-    
-    return "fill #{args[0]} #{DEFAULT_Y} #{z_coord} " \
-                "#{args[1]} #{DEFAULT_Y} #{z_coord} #{args[2]}"
-
-  end
-end
-
-
-
-
-
+__END__
 =begin
 [ lenght: WIDTH
   [ lenght: HEIGHT
@@ -43,7 +20,6 @@ end
 
 p filled_columns
 
-__END__
 IO.readlines("./frame.mcfunction").each do |line|
   p line
 end
