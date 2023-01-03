d728cb6c56ad04546ea28c5b4bcd484e3534d0f9
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2023-01-03 01:32:16 -0300

    fixed name

--- modules/optimize.rb
+++ modules/optimize.rb
@@ -9,7 +9,7 @@ module Optimize
   # z, Vec[x0, x1, block] => /fill || /setblock
   # 0, [0, 2, concrete 15] => "fill 0 50 0 2 50 0 concrete 15"
   # 1, [5, 5, concrete 0]  => "setblock 5 50 1 concrete 0"
-  def eval(z_coord, *args)
+  def self.eval(z_coord, *args)
     if args[0] == args[1]
       return "setblock #{args[0]} #{DEFAULT_Y} #{z_coord} #{args[2]}"
     end
