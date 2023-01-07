65a7ba67b667ea4f164fb6306d6700d665a8c2e2
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2023-01-07 02:08:29 -0300

    turn usual commands

---
+++ classes/array.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# set methods for manipulating vectors and matrices
+class Array
+  include Vector
+  include Command
+
+  def to_vector
+    self.map { |cmd| Vector.to_vector(cmd) }
+  end
+
+  def compact_matrix
+    Vector.compact(self)
+  end
+
+  def to_command
+    self.map { |vec| Command.to_command(vec) }
+  end
+end
