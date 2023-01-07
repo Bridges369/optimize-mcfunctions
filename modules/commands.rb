03eaa49bb8add5691b52e17c24374973e3b3d200
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2023-01-07 02:04:29 -0300

    refactoring the code

--- modules/commands.rb
+++ modules/commands.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Set of methods for manipulating commands
 module Command
   # Constants
 
@@ -7,14 +10,10 @@ module Command
   # [0, 2, 50, 0, "concrete 15"] => "fill 0 50 0 2 50 0 concrete 15"
   # [5, 5, 50, 0, "concrete 0"]  => "setblock 5 50 1 concrete 0"
   def self.to_command(vec)
-    if vec[0] == vec[1]
-      return "setblock %1s %1s %1s %1s" % vec[1..]
-    end
-    
-    return "fill %1s %1s %1s %1s %1s %1s %1s" % [
-      vec[0], *vec[2..3], vec[1], *vec[2..4]
+    return "setblock %1d %1d %1d %1s" % vec[1..] if vec[0] == vec[1]
+
+    return "fill %1d %1d %1d %1d %1d %1d %1s" % [
+      vec[0], *vec[2..3], vec[1], *vec[2..]
     ]
-
   end
-
 end
